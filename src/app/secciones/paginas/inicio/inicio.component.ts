d7c75a0d4509a2e8314295e5626f2bbdb56b8432
Franco Oliva <franco.oliva1991@gmail.com>
2021-07-06 13:42:23 -0300

    Pequeñas modificaciones en la cara de la web

--- src/app/secciones/paginas/inicio/inicio.component.ts
+++ src/app/secciones/paginas/inicio/inicio.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 export interface Card {
   titulo: string;
   descripcion: string;
+  btn: string;
 }
 
 @Component({
@@ -25,19 +26,23 @@ export class InicioComponent implements OnInit {
   cards: Card[] = [
     {
       titulo: 'Proyectos Personales',
-      descripcion: 'Esta parte hay que completarla con la descripción de la carta.'
+      descripcion: 'Actualmente estoy pasando una aplicación que desarrollé con Framework7 a Angular. Haciendo click en "Saber más..." vas a poder ver una descripción más detallada.',
+      btn: 'Ver Proyectos'
     },
     {
-      titulo: 'Proyectos Personales',
-      descripcion: 'Esta parte hay que completarla con la descripción de la carta.'
+      titulo: 'Aplicaciones Cursos Online',
+      descripcion: 'Una sección en donde muestro todas las aplicaciones o "mini aplicaciones" desarrolladas con cursos online. Siempre que aprendo algo nuevo, lo aplico.',
+      btn: 'Ver Aplicaciones'
     },
     {
-      titulo: 'Proyectos Personales',
-      descripcion: 'Esta parte hay que completarla con la descripción de la carta.'
+      titulo: 'Certificaciones',
+      descripcion: 'Proponerse algo, empezarlo y terminarlo, es muy importante para mi. Escuché y leí que estos "cartones" no sirven, pero en mi camino, son pequeños pasos.',
+      btn: 'Ver Certificados'
     },
     {
-      titulo: 'Proyectos Personales',
-      descripcion: 'Esta parte hay que completarla con la descripción de la carta.'
+      titulo: 'Información Personal',
+      descripcion: 'Una sección sencilla en donde hay información más detalla de quién soy, qué conocimientos tengo, experiencia laboral y formas de contactarme.',
+      btn: 'Saber más...'
     }
   ];
 
