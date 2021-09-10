fab1ed5b82868722eb99226127e7933076126332
Franco Oliva <franco.oliva1991@gmail.com>
2021-09-10 03:42:28 -0300

    agregamos hostel app y ejercicio curso angular

--- src/app/secciones/paginas/proyectos-personales/proyectos-personales.component.ts
+++ src/app/secciones/paginas/proyectos-personales/proyectos-personales.component.ts
@@ -5,6 +5,11 @@ import { Proyectos } from '../interfaces/proyectos.interface';
   selector: 'app-proyectos-personales',
   templateUrl: './proyectos-personales.component.html',
   styles: [
+    `
+    .text-decoration-none {
+      text-decoration: none;
+    }
+    `
   ]
 })
 export class ProyectosPersonalesComponent {
@@ -12,12 +17,14 @@ export class ProyectosPersonalesComponent {
   misProyectos: Proyectos[] = [
     {
       nombre: 'Hostel App',
-      descripcion: `Permite registrar clientes, pagos, gastos, reportes diarios y tener un control de las habitaciones y sus camas.
+      descripcion: `Permite registrar clientes, pagos, gastos, reportes y tener un control de las habitaciones y sus camas.
                     Este proyecto fue desarrollado con Framework7 en el curso intensivo de 3 meses en Comunidad IT. 
                     `,
       tecnologiasUtilizadas: 'Framework7, Firebase, Javascript, HTML, CSS.'
     }
-  ]
+  ];
+
+  
 
 
 }
